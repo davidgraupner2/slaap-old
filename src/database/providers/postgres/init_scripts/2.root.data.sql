aa4c0e0f2cbc77f990824e32eb93875b470906f5
David Graupner <david.graupner@gmail.com>
2022-11-21 07:18:02 +1100

    Added Default Application Name

--- src/database/providers/postgres/init_scripts/2.root.data.sql
+++ src/database/providers/postgres/init_scripts/2.root.data.sql
@@ -1,6 +1,7 @@
 -- Insert the Root User into the Users Table
-INSERT INTO public."user"(
-    username, password
-) values (
-    "root@localhost","r00tP@ssword1#@!"
-)
+INSERT INTO public."user"(username, password) 
+    VALUES ('root@localhost','r00tP@ssword1#@!');
+
+-- Insert the Root Application inot the Application Table
+INSERT INTO public.application(created_by, updated_by, name)
+    VALUES ((select id from public."user" where username='root@localhost'), (select id from public."user" where username='root@localhost'), 'Root');
