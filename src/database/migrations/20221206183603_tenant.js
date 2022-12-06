d8856ad9a08285d5ea6663baa9af733094abba19
David Graupner <david.graupner@gmail.com>
2022-12-07 06:53:40 +1100

    Added SchemaName to Tenant

--- src/database/migrations/20221206183603_tenant.js
+++ src/database/migrations/20221206183603_tenant.js
@@ -6,6 +6,7 @@ exports.up = function (knex) {
   return knex.schema.withSchema('public').createTable('tenant', function (table) {
     table.bigIncrements('id');
     table.string('name', 255).notNullable().unique();
+    table.string('schema_name', 255).notNullable().unique();
     table.string('description', 4000).nullable();
   });
 };
